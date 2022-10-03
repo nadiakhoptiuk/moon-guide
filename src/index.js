af557f484ed0409cd5e631518bb87a80c27ab664
Nadia Khoptiuk <holineinadia@gmail.com>
2022-10-03 11:53:16 +0300

    update package.json

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <HashRouter>
-        {/* <BrowserRouter> */}
+        {/* <BrowserRouter basename="https://nadiakhoptiuk/moon-guide/"> */}
         <App />
         {/* </BrowserRouter> */}
       </HashRouter>
